6707a11ed4becf8d4450491035bdbee9252ef3b7
Benjamin144 <josephroberts@email.com>
2022-05-08 21:23:26 +0000

    first stage web scrape project build

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const PORT = 8000
-const axios = require('axios') // to perform GET requests
-const cheerio = require('cheerio')
-const express = require('express')
+const PORT = 8000;
+const axios = require("axios"); // to perform GET requests
+const cheerio = require("cheerio");
+const express = require("express");
 const visitMalvern = express();
-const url = 'https://www.visitthemalverns.org/'
+const url = "https://www.visitthemalverns.org/";
 axios(url)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const articles = []
-        $('.gb-headline-text', html).each(function() {
-            const site = $(this).text()
-            const url = $(this).find('a').attr('href')
-            articles.push({
-                site,
-                url    
-            })
-        })
-        console.log(articles)
-    }).catch(err => console.log(err))
-visitMalvern.listen(PORT, () => console.log(`server listening to PORT ${PORT}`));
-
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articles = [];
+    $(".gb-headline-text", html).each(function () {
+      const site = $(this).text();
+      const url = $(this).find("a").attr("href");
+      articles.push({
+        site,
+        url,
+      });
+    });
+    console.log(articles);
+  })
+  .catch((err) => console.log(err));
+visitMalvern.listen(PORT, () =>
+  console.log(`server listening to PORT ${PORT}`)
+);
